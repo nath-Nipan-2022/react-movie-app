e576c866e81920576a77657eb8c91217dcb91cef
Nipan Debnath <debnathnipan719@gmail.com>
2023-09-18 00:01:55 +0530

    fixed the issue of duplicate id's

--- src/pages/SearchResults/SearchResults.jsx
+++ src/pages/SearchResults/SearchResults.jsx
@@ -4,13 +4,13 @@ import Container from "../../components/Container";
 import MovieCard from "../../components/MovieCard";
 import Skeletons from "../../components/Skeletons";
 import { useGetSearchResultsQuery } from "../../store/apis/moviesApi";
+import { useInfiniteScroll } from "../../hook/useInfiniteScroll";
 
 const SearchResults = () => {
   const [pageNum, setPageNum] = useState(1);
   const [results, setResults] = useState([]);
   const [hasMore, setHasMore] = useState(true);
   const { query } = useParams();
-  const observer = useRef();
 
   const { data, isFetching, error } = useGetSearchResultsQuery({
     query,
@@ -21,12 +21,10 @@ const SearchResults = () => {
     if (pageNum === data?.total_pages) {
       setHasMore(false);
     }
-    if (data?.results.length > 0) {
+    if (!isFetching) {
       setResults((prev) => [...prev, ...data.results]);
-    } else {
-      setResults(data?.results);
     }
-  }, [pageNum, data]);
+  }, [pageNum, data, isFetching]);
 
   // reset results
   useEffect(() => {
@@ -34,19 +32,14 @@ const SearchResults = () => {
     setPageNum(1);
   }, [query]);
 
-  const lastElementRefFunc = useCallback(
-    (node) => {
-      if (isFetching) return;
-      if (observer.current) observer.current.disconnect();
+  const handleInfiniteScroll = () => {
+    setPageNum((prev) => prev + 1);
+  };
 
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          setPageNum((prev) => prev + 1);
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [isFetching, hasMore]
+  const { lastElementRefFunc } = useInfiniteScroll(
+    handleInfiniteScroll,
+    isFetching,
+    hasMore
   );
 
   const finalResults = results?.length > 0 ? results : data?.results;
@@ -59,7 +52,7 @@ const SearchResults = () => {
           movie={r}
           endpoint={r.endpoint}
           ref={lastElementRefFunc} //👈 lastElemRef
-          className="shrink-0 bg-skeleton aspect-[1/1.45]"
+          className="shrink-0 bg-skeleton aspect-[1/1.35]"
         />
       );
     }
@@ -76,20 +69,16 @@ const SearchResults = () => {
 
   return (
     <main>
-      <section className="pt-6 pb-12">
+      <section className="pb-12">
         <Container className={"px-8"}>
           <div className="py-2 mb-2 font-medium">
             Search results for {`'${query}'`}
           </div>
           <div className="grid grid-cols-2 gap-4 mt-4 sm:grid-cols-3 sm:gap-6 lg:gap-4 lg:grid-cols-4">
             {renderResults}
+            {isFetching && renderSkeletons}
           </div>
-          {isFetching && (
-            <div className="grid grid-cols-2 gap-4 mt-4 sm:grid-cols-3 sm:gap-6 lg:gap-4 lg:grid-cols-4 min-h-[400px]">
-              {renderSkeletons}
-            </div>
-          )}
-          {results?.length <= 0 && <div>No results found</div>}
+          {!isFetching && results?.length <= 0 && <div>No results found</div>}
           {error && (
             <div className="font-bold text-center text-red-500">{error}</div>
           )}
@@ -99,10 +88,10 @@ const SearchResults = () => {
   );
 };
 
-const renderSkeletons = Array(4)
+const renderSkeletons = Array(5)
   .fill(0)
   .map((_, i) => (
-    <div key={i} className="rounded-xl aspect-[1/1.5] relative bg-[#d49aff69]">
+    <div key={i} className="rounded-xl aspect-[1/1.35] relative bg-[#d49aff69]">
       <article className="absolute inset-0 flex flex-col justify-between p-4 rounded-lg">
         <Skeletons className="w-10 h-3" />
         <div>
