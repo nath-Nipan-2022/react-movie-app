7adbe3dde5020089f6a8dfdc011facdce8929794
Nipan Debnath <debnathnipan719@gmail.com>
2023-09-18 02:10:07 +0530

    watchList reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { moviesApi } from "./apis/moviesApi";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { moviesSlice } from "./slices/moviesSlice";
+import { watchListSlice } from "./slices/watchListSlice";
 
 const store = configureStore({
   reducer: {
     [moviesApi.reducerPath]: moviesApi.reducer,
     ImagesUrls: moviesSlice.reducer,
+    watchList: watchListSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(moviesApi.middleware),
