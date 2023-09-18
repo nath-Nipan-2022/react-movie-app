08c98ad4e79bfae56fa6d971a83e98d748f7ed42
Nipan Debnath <debnathnipan719@gmail.com>
2023-09-18 23:59:45 +0530

    update

--- src/pages/SearchResults/SearchResults.jsx
+++ src/pages/SearchResults/SearchResults.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Container from "../../components/Container";
 import MovieCard from "../../components/MovieCard";
@@ -22,13 +22,16 @@ const SearchResults = () => {
       setHasMore(false);
     }
     if (!isFetching) {
-      setResults((prev) => [...prev, ...data.results]);
+      if (pageNum !== 1) {
+        setResults((prev) => [...prev, ...data.results]);
+        return;
+      }
+      setResults(data.results);
     }
   }, [pageNum, data, isFetching]);
 
   // reset results
   useEffect(() => {
-    setResults([]);
     setPageNum(1);
   }, [query]);
 
@@ -70,11 +73,11 @@ const SearchResults = () => {
   return (
     <main>
       <section className="pb-12">
-        <Container className={"px-8"}>
+        <Container>
           <div className="py-2 mb-2 font-medium">
             Search results for {`'${query}'`}
           </div>
-          <div className="grid grid-cols-2 gap-4 mt-4 sm:grid-cols-3 sm:gap-6 lg:gap-4 lg:grid-cols-4">
+          <div className="grid grid-cols-2 gap-4 mt-4 sm:grid-cols-3 md:grid-cols-4 sm:gap-6 lg:gap-4 lg:grid-cols-5">
             {renderResults}
             {isFetching && renderSkeletons}
           </div>
